b0ed529e4b2740632716ba3470e18954115438c3
Max Tyler <mtyler88@gmail.com>
2017-03-29 21:00:27 +0100

    Add files for energy 0.236

--- plumed/wall_0.236/sum_hills.sh
+++ plumed/wall_0.236/sum_hills.sh
@@ -1,3 +1,3 @@
-#plumed sum_hills --hills hills_124.dat --outfile fes_124_ --kt 1.031 --mintozero --stride 500
-plumed sum_hills --histo new_colvar.dat --kt 1.031 --idw all,alpha_beta --sigma 2,2 --outhisto histo_all_core.dat
+#plumed sum_hills --hills hills_124.dat --outfile fes_124_ --idw all --kt 1.031 --mintozero --stride 500
+plumed sum_hills --histo colvar.dat --kt 1.031 --idw beta_beta,alpha_beta --sigma 1,1 --outhisto histo_beta_core.dat
 
